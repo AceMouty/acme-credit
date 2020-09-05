50bf3f92ec5e8562c5e38217dcf4f434772457f7
AceMouty <kyleguerrero85@gmail.com>
2020-09-05 16:14:41 -0500

    style: added in Material Table module
    added in the material table module to be used in the loan component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule, FormsModule } from "@angular/forms"
-
 import { AppComponent } from './app.component';
-
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 // Custom Components
@@ -17,6 +15,7 @@ import { LoanApplicationService } from "./services/loanApplications.service"
 import { MatFormFieldModule } from "@angular/material/form-field"
 import { MatInputModule } from "@angular/material/input"
 import { MatButtonModule } from "@angular/material/button"
+import { MatTableModule } from "@angular/material/table"
 
 @NgModule({
   declarations: [
@@ -31,7 +30,8 @@ import { MatButtonModule } from "@angular/material/button"
     FormsModule,
     MatFormFieldModule,
     MatInputModule,
-    MatButtonModule
+    MatButtonModule,
+    MatTableModule
   ],
   providers: [
     LoanApplicationService,

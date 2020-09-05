f1776c2b039d7790af5107f48fe83c0846a239c3
AceMouty <kyleguerrero85@gmail.com>
2020-09-05 01:02:50 -0500

    chore: removed dead code

--- src/app/components/loan-application/loanApplication.component.ts
+++ src/app/components/loan-application/loanApplication.component.ts
@@ -7,29 +7,6 @@ import { NgForm } from '@angular/forms';
     templateUrl: "./loanApplication.component.html",
     styleUrls: ['./loanApplication.component.css']
 })
-// export class LoanApplicationComponent {
-//     name: string = "Loan Application"
-
-//     // Populates on form submission
-//     applicationInfo;
-
-//     constructor(private formBuilder: FormBuilder){
-//         this.applicationInfo = this.formBuilder.group({
-//             name: "",
-//             email: "",
-//             phone: "",
-//             loanAmount: ""
-//         })
-//     }
-
-//     // Handle form submission
-//     submitHandler(e, formData){
-//         e.preventDefault()
-//         this.applicationInfo.reset();
-//         console.log("Form Submitted", formData);
-//     }
-// }
-
 export class LoanApplicationComponent {
 
     name = "Loan Application"

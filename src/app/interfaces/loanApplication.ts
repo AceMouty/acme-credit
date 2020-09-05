e746dd14a54b2df0b6c86ff8acdb0aa3646313aa
AceMouty <kyleguerrero85@gmail.com>
2020-09-05 15:02:18 -0500

    feat: created loan interface
    loan interface created to enforce needed fields in a loan application

---
+++ src/app/interfaces/loanApplication.ts
@@ -0,0 +1,9 @@
+
+// Describe the shape of ALL loan applications
+export interface LoanApplication {
+    id: number,
+    applicantName: string,
+    applicantEmail: string,
+    applicantPhoneNumber: string,
+    loanAmount: number
+}

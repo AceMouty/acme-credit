e68336c5bb5e9924df57cf20c6df1a143a036b25
AceMouty <kyleguerrero85@gmail.com>
2020-09-04 22:58:39 -0500

    feat: created a submit handler
    the submit handler is called on form submission and currently logs
    the data that is passed from the form

--- src/app/components/loan-application/loanApplication.component.ts
+++ src/app/components/loan-application/loanApplication.component.ts
@@ -1,9 +1,42 @@
 import { Component } from "@angular/core"
+import { NgForm } from '@angular/forms';
+// import { FormBuilder } from "@angular/forms";
 
 @Component({
     selector: "app-loanApplication",
-    templateUrl: "./loanApplication.component.html"
+    templateUrl: "./loanApplication.component.html",
+    styleUrls: ['./loanApplication.component.css']
 })
+// export class LoanApplicationComponent {
+//     name: string = "Loan Application"
+
+//     // Populates on form submission
+//     applicationInfo;
+
+//     constructor(private formBuilder: FormBuilder){
+//         this.applicationInfo = this.formBuilder.group({
+//             name: "",
+//             email: "",
+//             phone: "",
+//             loanAmount: ""
+//         })
+//     }
+
+//     // Handle form submission
+//     submitHandler(e, formData){
+//         e.preventDefault()
+//         this.applicationInfo.reset();
+//         console.log("Form Submitted", formData);
+//     }
+// }
+
 export class LoanApplicationComponent {
-    name: string = "Loan Application"
+
+    name = "Loan Application"
+    constructor() { }
+
+    onSubmit(formObj: NgForm) {
+        console.log("FORM SUBMITTED!");
+        console.log(formObj.value)
+    }
 }

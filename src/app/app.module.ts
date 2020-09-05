2648c30f6f1944b5279dfc74d1812f8f62c2c62c
AceMouty <kyleguerrero85@gmail.com>
2020-09-04 22:59:53 -0500

    chore: imported needed Material modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule, FormsModule } from "@angular/forms"
 
 import { AppComponent } from './app.component';
 
 // Custom Components
 import { LoanApplicationComponent } from "./components/loan-application/loanApplication.component";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+
+// Material Components
+import { MatFormFieldModule } from "@angular/material/form-field"
+import { MatInputModule } from "@angular/material/input"
+import { MatButtonModule } from "@angular/material/button"
 
 @NgModule({
   declarations: [
@@ -14,7 +20,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]

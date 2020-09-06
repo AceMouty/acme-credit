c505ca9d499c52f80fb4c177faacc41ed59ac3fc
AceMouty <kyleguerrero85@gmail.com>
2020-09-05 21:16:58 -0500

    feat: added dynamic routing
    added dynamic routing for existing loans so that a user can
    view / edit a given application

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,14 @@ import { RouterModule, Routes } from "@angular/router"
 // Components
 import { LoansComponent } from './components/loans/loans.component'
 import { LoanApplicationComponent } from './components/loan-application/loanApplication.component'
+import { LoanEditorComponent } from "./components/loan-editor/loanEditor.component"
 
 
 const routes: Routes = [
   { path: "", redirectTo: "/loans", pathMatch: "full" },
   { path: "loans", component: LoansComponent },
   { path: "loan-application", component: LoanApplicationComponent },
+  { path: "loan-editor/:id", component: LoanEditorComponent },
 ]
 
 @NgModule({

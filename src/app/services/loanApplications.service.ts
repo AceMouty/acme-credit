2e2edca911c9e825ff1ef144510d9c98cb591bfb
AceMouty <kyleguerrero85@gmail.com>
2020-09-06 14:40:06 -0500

    feat: setup http requests
    setup network requests to use the memory server
    firebase API has been completley removed
    
    service is talking to the memory server correctly
    and serving all views correctly

--- src/app/services/loanApplications.service.ts
+++ src/app/services/loanApplications.service.ts
@@ -2,54 +2,51 @@ import { Injectable } from "@angular/core"
 import { APPLICATIONS } from "../utils/loanApplications";
 import { LoanApplication } from "../interfaces/loanApplication"
 import { Observable, of } from "rxjs"
-import { map } from "rxjs/operators"
-import { HttpClient } from '@angular/common/http';
+import { map, tap } from "rxjs/operators"
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
     providedIn: "root"
 })
 export class LoanApplicationService {
 
-    private baseUrl: string = "https://acme-credit.firebaseio.com"
+    // URL for Memory server, swap with API when able
+    private baseUrl: string = "api/applications";
+
+    // Header config
+    httpOptions = {
+        headers: new HttpHeaders({ "Content-Type": "application/json" })
+    }
+
     constructor(private http: HttpClient) { }
 
     getApplications(): Observable<LoanApplication[]> {
         //TODO: make a network request to get all applications from API
         // return of(APPLICATIONS)
-
-        // FIREBASE API: get all the loan applications and convert the response from
-        // a object to an array of objects
-        return this.http.get(`${this.baseUrl}/posts.json`)
-            .pipe(map(responseData => {
-                const applicationsArray: LoanApplication[] = []
-                for (const key in responseData) {
-                    applicationsArray.push(...responseData[key])
-                }
-                return applicationsArray
-            }))
+        return this.http.get<LoanApplication[]>(this.baseUrl)
     }
 
-    getApplication(id: number): Observable<LoanApplication> {
-        return of(APPLICATIONS.find(loan => loan.id === id))
+    getApplication(id: string): Observable<LoanApplication> {
+        // return of(APPLICATIONS.find(loan => loan.id === id))
+        return this.http.get<LoanApplication>(`${this.baseUrl}/${id}`)
     }
 
-    updateLoan(newLoan: LoanApplication): void {
+    updateLoan(newLoan: LoanApplication): Observable<any> {
         //TODO: send updated data to the DB and make this func use rxjs
 
-        APPLICATIONS.forEach(loan => {
-            if (loan.id === newLoan.id) {
-                console.log("FOUND LOAN", loan)
-                loan = { ...newLoan }
-                console.log("UPDATED LOAN", loan)
-            }
-        })
+        // APPLICATIONS.forEach(loan => {
+        //     if (loan.id === newLoan.id) {
+        //         console.log("FOUND LOAN", loan)
+        //         loan = { ...newLoan }
+        //         console.log("UPDATED LOAN", loan)
+        //     }
+        // })
+        return this.http.put(this.baseUrl, newLoan, this.httpOptions)
 
     }
 
-    createApplication(appData: LoanApplication): void {
-        // console.log("FROM LOAN SERVICE: ", appData)
-        this.http.post(`${this.baseUrl}/posts.json`, appData)
-            .subscribe(resData => console.log(resData))
+    createApplication(appData: LoanApplication): Observable<LoanApplication> {
+        return this.http.post<LoanApplication>(this.baseUrl, appData, this.httpOptions)
     }
 
 }
